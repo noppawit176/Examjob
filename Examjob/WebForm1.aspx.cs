29007f39b41f254bcc7896ccfec10491730d6bb3
Noppawit Titijantawat <noppawit176@gmail.com>
2019-07-15 03:13:18 +0700

    debug http not exits

---
+++ Examjob/WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+namespace Examjob
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String seach = txtName.Text;
+            Response.Write(seach);
+            this.table.Text = seach;
+
+
+        }
+    }
+}
